619ee88d35aee695168c26499c4910f285f1979b
Vladyslav Yevlampiev <159730014+gekkoUA@users.noreply.github.com>
2026-04-26 15:08:53 +0300

    added control mechanics to the vehicle
    - When the player presses the up/down arrows, the vehicle will move forward and backward.
    - When the player presses the left/right arrows, the vehicle turns.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,7 +2,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5.0f;
+    //Private Variables
+    private float speed = 5.0f;
+    private float turnSpeed = 30.0f;
+    private float horizontalInput;
+    private float verticalInput;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,7 +16,12 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        // This is where we get player input
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        // Moves the vehicle forward based on vertical input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+        // Rotates the vehicle based on horizontal input
+        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
